3cb7654e99bff6df98ea46eb6d69ab09712337a1
Atharva Jain <atharvajain21@gmail.com>
2026-09-11 19:12:19 -0500

    Created home card

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,12 @@
 /* Your JS here. */
-console.log('Hello World!')
+const typewriterText = "A Computer Science major at UIUC! 👋 ";
+const target = document.getElementById("typed-text");
+let i = 0;
+
+(function typewriter() {
+    if (i > typewriterText.length) {
+        return;
+    }
+    target.textContent = typewriterText.slice(0, i++);
+    setTimeout(typewriter, 60);
+})();
